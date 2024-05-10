a3306f23eb8f00f9425bc7d6f9e0aa44c030b3ef
tasnuvac <tasnuvac4@gmail.com>
2024-05-10 02:22:45 -0400

    Created a player

--- Final Project/Assets/Scripts/CoinBehavior.cs
+++ Final Project/Assets/Scripts/CoinBehavior.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class CoinBehavior : MonoBehaviour
 {
+    public int RotationSpeed = 100;
+    private Transform coinTransform;
     // Start is called before the first frame update
     void Start()
     {
-        
+        coinTransform = this.GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        coinTransform.Rotate(RotationSpeed * Time.deltaTime, 0, 0);
     }
 }
